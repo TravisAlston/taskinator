a3207300bfa11525ddbcb4e11179a45a37c3674c
Valdeth <alstontravis6@gmail.com>
2021-02-18 00:56:19 -0700

    add naming functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
-let taskItemEl = document.createElement("li");
-taskItemEl.textContent = "hello";
-taskItemEl.appendChild(taskItemEl);
-taskItemEl.className = "task-item";
-
-let tasksToDoEl = document.querySelector('#tasks-to-do');
-
 let formEl= document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
 
     event.preventDefault();
+    let taskNameInput= doncument.querySelector("input[name= 'task-name']").value;
+    let taskTypeInput= document.querySelector("select[name='task-type']").value;
 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    let taskInfoEl= document.createElement("div");
+    taskInforEl.className= "task-info";
+
+    taskInfoEl.innerHTML= "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class= 'task-type'>" + taskTypeINput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 };
 
